8bf3a49f0931daf05b85cb29487b45cc683361b1
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2024-03-11 22:15:24 +0200

    initializing the project / added some global styles

---
+++ Pawprints - FE/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HomeComponent } from './core/home/home.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'Pawprints';
+}
