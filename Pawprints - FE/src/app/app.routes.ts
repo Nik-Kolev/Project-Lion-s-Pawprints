4c628edc2824fab94cf1452b85929879995a8e73
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2024-03-18 13:30:52 +0200

    register component html/css done

--- Pawprints - FE/src/app/app.routes.ts
+++ Pawprints - FE/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './core/home/home.component';
 import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
   },
 ];
