5d0489849687cc918f5da1271c03e394eac04755
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2024-03-19 19:04:14 +0200

    toastr added + httpclinetmodule

--- Pawprints - FE/src/app/app.component.ts
+++ Pawprints - FE/src/app/app.component.ts
@@ -3,13 +3,22 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { HomeComponent } from './core/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HttpClientModule,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  providers: [AuthService],
 })
 export class AppComponent {
   title = 'Pawprints';
--- Pawprints - FE/src/app/app.config.ts
+++ Pawprints - FE/src/app/app.config.ts
@@ -3,7 +3,17 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()],
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideToastr({
+      timeOut: 3000,
+      preventDuplicates: true,
+      positionClass: 'toast-top-right',
+      closeButton: true,
+    }),
+  ],
 };
