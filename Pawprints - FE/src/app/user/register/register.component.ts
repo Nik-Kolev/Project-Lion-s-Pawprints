d80772c29c8a09727bd3e062a9605f9255e1d429
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2024-03-20 17:48:58 +0200

    register component updated with error handling

--- Pawprints - FE/src/app/user/register/register.component.ts
+++ Pawprints - FE/src/app/user/register/register.component.ts
@@ -9,48 +9,69 @@ import {
 import { emailValidator } from '../../validators/email.validator';
 import { rePassChecker } from '../../validators/rePass.validator';
 import { dataTrimmer } from '../../shared/trimmer';
+import { AuthService } from '../../services/auth.service';
+import { Router, RouterLink } from '@angular/router';
+import { ErrorsComponent } from '../../shared/errors/errors.component';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink, ErrorsComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
   registerForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-    this.registerForm = this.fb.group(
-      {
-        email: new FormControl('', [
-          emailValidator.required,
-          emailValidator.isValidEmail,
-        ]),
-        password: ['', Validators.required],
-        rePassword: ['', Validators.required],
-      },
-      { validators: [rePassChecker('password', 'rePassword')] }
-    );
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private toast: ToastrService,
+    private route: Router
+  ) {
+    this.registerForm = this.fb.group({
+      email: new FormControl('', [
+        emailValidator.required,
+        emailValidator.isValidEmail,
+      ]),
+      passGroup: this.fb.group(
+        {
+          password: ['', [Validators.required, Validators.minLength(5)]],
+          rePassword: ['', [Validators.required, Validators.minLength(5)]],
+        },
+        {
+          validators: [rePassChecker('password', 'rePassword')],
+        }
+      ),
+    });
   }
 
   onSubmit(): void {
+    this.registerForm.markAllAsTouched();
+
     const trimmedData = dataTrimmer(this.registerForm.value);
     this.registerForm.setValue(trimmedData, { emitEvent: false });
 
-    if (this.registerForm.errors) {
-      console.log('Form errors', this.registerForm.errors);
-    }
-
-    if (this.registerForm.valid) {
-      console.log(this.registerForm.value);
+    if (!this.registerForm.valid) {
+      return;
     } else {
-      Object.keys(this.registerForm.controls).forEach((x) => {
-        let formErrors = this.registerForm.get(x)?.errors;
-        if (formErrors) {
-          console.log(`${x} errors`, formErrors);
-        }
-      });
+      this.auth
+        .register(
+          this.registerForm.value.email,
+          this.registerForm.get('passGroup.password')?.value,
+          this.registerForm.get('passGroup.rePassword')?.value
+        )
+        .subscribe({
+          next: () => {
+            this.toast.success('Registered successfully.');
+            this.route.navigate(['/']);
+          },
+          error: (error) => {
+            console.log(error.error);
+            this.toast.error(error.error.error);
+          },
+        });
     }
   }
 }
