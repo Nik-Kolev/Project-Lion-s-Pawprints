036dbe18f316a7d93e9f8c7f738a66c9419ca51e
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2024-03-31 11:16:06 +0300

    error page done

--- Pawprints - FE/src/app/app.routes.ts
+++ Pawprints - FE/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { RegisterComponent } from './user/register/register.component';
 import { DisplaySafariComponent } from './safaris/display-safari/display-safari.component';
 import { CreateSafariComponent } from './safaris/create-safari/create-safari.component';
 import { CatalogSafariComponent } from './safaris/catalog-safari/catalog-safari.component';
+import { ErrorComponent } from './core/error/error.component';
+import { guestGuard } from './guards/guest.guard';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -15,14 +18,17 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+    canActivate: [guestGuard],
   },
   {
     path: 'register',
     component: RegisterComponent,
+    canActivate: [guestGuard],
   },
   {
     path: 'createSafari',
     component: CreateSafariComponent,
+    canActivate: [authGuard],
   },
   {
     path: 'catalogSafari',
@@ -35,5 +41,10 @@ export const routes: Routes = [
   {
     path: 'editSafari/:safariId',
     component: CreateSafariComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: '**',
+    component: ErrorComponent,
   },
 ];
---
+++ Pawprints - FE/src/app/core/error/error.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-error',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './error.component.html',
+  styleUrl: './error.component.scss',
+})
+export class ErrorComponent {}
